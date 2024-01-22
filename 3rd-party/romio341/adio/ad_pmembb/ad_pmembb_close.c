fae517b548bc502a54f3e26f95d8d285e820d13c
range3 <white.line.oar@gmail.com>
2024-01-22 15:30:25 +0900

    ad_pmembb: quit sync on close.

--- 3rd-party/romio341/adio/ad_pmembb/ad_pmembb_close.c
+++ 3rd-party/romio341/adio/ad_pmembb/ad_pmembb_close.c
@@ -13,11 +13,11 @@ void ADIOI_PMEMBB_Close(ADIO_File fd, int *error_code)
     DEBUG_PRINT(fd->comm, fd->filename);
 
     if(handler != NULL) {
-      ret = rpmbb_bb_sync(handler);
-      if (ret != 0) {
-          *error_code = ADIOI_Err_create_code(__func__, fd->filename, -ret);
-          DEBUG_PRINT(fd->comm, "rpmbb_bb_sync failed");
-      }
+      // ret = rpmbb_bb_sync(handler);
+      // if (ret != 0) {
+      //     *error_code = ADIOI_Err_create_code(__func__, fd->filename, -ret);
+      //     DEBUG_PRINT(fd->comm, "rpmbb_bb_sync failed");
+      // }
       rpmbb_bb_close(handler);
     }
 
